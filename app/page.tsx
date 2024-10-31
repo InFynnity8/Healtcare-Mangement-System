5296b0beded16db70144a5158a33941da584cf2c
InFynnity8 <sackeyemmanuelfynn@gmail.com>
2024-10-31 15:10:07 +0000

    HomePage done

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,39 @@
-
-import{ Button } from "@/components/ui/button"
+import PatientForm from "@/components/foms/PatientForm";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-   <div>
-    <h1>Home</h1> 
-    <Button>Hi</Button>
-   </div>
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar my-auto container">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="patient"
+            className="mb-12 h-10 w-fit"
+          />
+
+          <PatientForm />
+
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              © 2024 CarePulse
+            </p>
+            <Link href="/?amin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
+        </div>
+      </section>
+      <Image
+        src="/assets/images/onboarding-img.png"
+        width={1000}
+        height={1000}
+        alt="patient"
+        className="side-img max-w-[50%]"
+      />
+    </div>
   );
 }
